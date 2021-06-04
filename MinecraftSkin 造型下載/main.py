95a5853cbe733cd2f83014f2e535e053657d1973
Mango <evan940415@gmail.com>
2021-06-04 16:17:58 +0800

    .

--- MinecraftSkin 造型下載/main.py
+++ MinecraftSkin 造型下載/main.py
@@ -3,14 +3,14 @@ from requests import get
 from base64 import b64decode
 
 while True:
-    mcid = input('Minecraft ID:') or 'evan_hsieh'
+    mcid = input('Minecraft ID:') or -1
     try:
         uuid = loads(get(url=f'https://api.mojang.com/users/profiles/minecraft/{mcid}').content.decode('UTF-8'))
     except:
         print('ID 不存在')
     else:
         skinbase64 = loads(get(url=f'https://sessionserver.mojang.com/session/minecraft/profile/{uuid["id"]}').content.decode('UTF-8'))['properties'][0]['value']
-        skin = eval(b64decode(skinbase64))
+        skin = loads(b64decode(skinbase64))
         filename = f'{mcid}.png'
         with open(filename, mode='wb') as pic:
             pic.write(get(skin['textures']['SKIN']['url']).content)
--- Verification code function 驗證碼函數/simple version - 純數字版/main.py
+++ Verification code function 驗證碼函數/simple version - 純數字版/main.py
@@ -1,18 +1,7 @@
 import random
 
-def verify(mode:str='info', length=16):
-    modeList = ['info', 'get']
-    if mode in modeList:
-        if mode == 'info':
-            output = 'info'
-        elif mode == 'get':
-            if length.isdigit():
-                codes = ''
-                for i in range(int(length)):
-                    codes += str(random.randint(0,9))
-                output = codes
-            else:
-                output = '[ERROR] length must be an integer'
-    else:
-        output = '[ERROR] mode not found'
-    return output
+def verify(length=16):
+    codes = ''
+    for i in range(int(length)):
+        codes += str(random.randint(0,9))
+    return codes
